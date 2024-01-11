02638281360669aa817d6c377676eb2f59ed2188
Riccardo Potalivo <riccardopotalivo@gmail.com>
2024-01-11 19:58:36 +0100

    controller type & character complete

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Type;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreTypeRequest;
+use App\Http\Requests\UpdateTypeRequest;
 
 class TypeController extends Controller
 {
@@ -15,27 +17,38 @@ class TypeController extends Controller
         $types = Type::all();
 
         return view('types.index', compact('types'));
-
     }
 
     /**
      * Show the form for creating a new resource.
+     *
      */
     public function create()
     {
-        //
+        return view('types.create');
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
      */
-    public function store(Request $request)
+    public function store(StoreTypeRequest $request)
     {
-        //
+        $formData = $request->validated();
+
+        $newType = Type::create($formData);
+
+        return to_route('types.show', $newType->id);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\View\View;
+     *
      */
     public function show(Type $type)
     {
@@ -44,25 +57,44 @@ class TypeController extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Type  $type
+     *
      */
     public function edit(Type $type)
     {
-        //
+        return view('types.edit', compact('type'));
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
+     *
      */
-    public function update(Request $request, Type $type)
+    public function update(UpdateTypeRequest $request, Type $type)
     {
-        //
+        $formData = $request->validated();
+
+        $type->fill($formData);
+
+        $type->update();
+
+        return to_route('types.show', $type->id);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Type  $type
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Type $type)
     {
-        //
+        $type->delete();
+
+        return to_route('types.index')->with('message', "il prodotto $type->title è stato eliminato");
     }
 }
