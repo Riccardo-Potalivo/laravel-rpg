5704559ed7ac6e75416dd9fd037dca969068f211
chiara1911 <chiaracesari89@gmail.com>
2024-01-11 20:36:37 +0100

    cancel required

--- resources/views/characters/create.blade.php
+++ resources/views/characters/create.blade.php
@@ -24,7 +24,7 @@
                             <div class="mb-3">
                                 <label for="name" class="form-label">Name</label>
                                 <input type="text" class="form-control @error('name') is-invalid @enderror"
-                                    id="name" name="name" value="{{ old('name') }}" required>
+                                    id="name" name="name" value="{{ old('name') }}">
                                 @error('name')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -46,7 +46,7 @@
                                 <label for="type_id" class="form-label">Type id</label>
 
                                 <input type="text" id="type_id" name="type_id" value="{{ old('type_id') }}"
-                                    class="form-control @error('type_id') is-invalid @enderror" required>
+                                    class="form-control @error('type_id') is-invalid @enderror" >
                                 @error('type_id')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -56,7 +56,7 @@
                                 <label for="attack" class="form-label">Attack</label>
 
                                 <input type="text" id="attack" name="attack" value="{{ old('attack') }}"
-                                    class="form-control @error('attack') is-invalid @enderror" required>
+                                    class="form-control @error('attack') is-invalid @enderror" >
                                 @error('attack')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -66,7 +66,7 @@
                                 <label for="defence" class="form-label">Defence</label>
 
                                 <input type="text" id="defence" name="defence" value="{{ old('defence') }}"
-                                    class="form-control @error('defence') is-invalid @enderror" required>
+                                    class="form-control @error('defence') is-invalid @enderror" >
                                 @error('defence')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -76,7 +76,7 @@
                                 <label for="speed" class="form-label">Speed</label>
 
                                 <input type="text" id="speed" name="speed" value="{{ old('speed') }}"
-                                    class="form-control @error('speed') is-invalid @enderror" required>
+                                    class="form-control @error('speed') is-invalid @enderror" >
                                 @error('speed')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
@@ -86,7 +86,7 @@
                                 <label for="life" class="form-label">Life</label>
 
                                 <input type="text" id="life" name="life" value="{{ old('life') }}"
-                                    class="form-control @error('life') is-invalid @enderror" required>
+                                    class="form-control @error('life') is-invalid @enderror">
                                 @error('life')
                                     <div class="invalid-feedback">{{ $message }}</div>
                                 @enderror
