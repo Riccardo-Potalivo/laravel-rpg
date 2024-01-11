ed79ed201f584fa3a6b387be2fa8861d88a1d950
ericaostini <ericaostini14@gmail.com>
2024-01-11 16:33:55 +0100

    edit file ItemSeeder

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,14 +4,28 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Item;
 
 class ItemSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
+        $json = file_get_contents(__DIR__ . '/data/items.json');
+        $items = json_decode($json, true);
 
+        foreach ($items as $itemData) {
+            $item = new Item();
+            $item->name = $itemData['name'];
+            $item->description = $itemData['description'];
+            $item->slug = $itemData['slug'];
+            $item->category = $itemData['category'];
+            $item->type = $itemData['type'];
+            $item->weight = $itemData['weight'];
+            $item->cost = $itemData['cost'];
+            $item->save();
+        }
     }
 }
