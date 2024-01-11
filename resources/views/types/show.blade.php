ea490527aecd20838dda0d46a32bb9051a46dcde
Finnedorn <daniele.giuntoli@outlook.com>
2024-01-12 00:01:46 +0100

    added new edit and create buttons on index and show pages, minor fix to card structure

--- resources/views/types/show.blade.php
+++ resources/views/types/show.blade.php
@@ -6,9 +6,17 @@
         <div id="character-detail" class="container py-5">
             <div class="row pt-5">
                 <div class="col-12 col-lg-8">
-                    <h1 class="pb-4">
-                        {{ $type->name }}
-                    </h1>
+                    <div class="d-flex justify-content-between  align-items-center ">
+                        <h1 class="pb-4">
+                            {{ $type->name }}
+                        </h1>
+                        {{-- bottone di edit --}}
+                        <a href="{{route('types.edit', $type->id)}}">
+                            <button class="btn btn-success rounded-3 border-0">
+                                <i class="fa-solid fa-pen" style="font-size: 0.7rem"></i>
+                            </button>
+                        </a>
+                    </div>
                     <h4 class="py-3">
                         Description
                     </h4>
