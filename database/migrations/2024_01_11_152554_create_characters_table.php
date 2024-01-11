0a030b07cde7a879697ce446a410fdce925b22cc
giuliabaratella <giuliabaratella7@gmail.com>
2024-01-11 17:14:28 +0100

    added character seeder content / added character migation content

--- database/migrations/2024_01_11_152554_create_characters_table.php
+++ database/migrations/2024_01_11_152554_create_characters_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,14 @@ return new class extends Migration
     {
         Schema::create('characters', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 200);
+            $table->text('description')->nullable();
+            $table->tinyInteger('type_id');
+            $table->tinyInteger('attack');
+            $table->tinyInteger('defence');
+            $table->tinyInteger('speed');
+            $table->tinyInteger('life');
+
             $table->timestamps();
         });
     }
--- database/seeders/CharacterSeeder.php
+++ database/seeders/CharacterSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Character;
 
 class CharacterSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class CharacterSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $json = file_get_contents(__DIR__ . 'data/characters.json');
+        $data = json_decode($json, true);
+
+        foreach ($data as $item) {
+            $new_character = new Character();
+            $new_character->name = $item["name"];
+            $new_character->description = $item["description"];
+            $new_character->type_id = $item["type_id"];
+            $new_character->attack = $item["attack"];
+            $new_character->defence = $item["defence"];
+            $new_character->speed = $item["speed"];
+            $new_character->life = $item["life"];
+            $new_character->save();
+        }
+
     }
 }
