0315916b911048eba8dc7407b383fc63dc8c711c
giuliabaratella <giuliabaratella7@gmail.com>
2024-01-15 19:16:35 +0100

    updated types forms

--- resources/views/admin/types/edit.blade.php
+++ resources/views/admin/types/edit.blade.php
@@ -48,14 +48,14 @@
 
                             <div class="d-flex">
                                 <div class="media me-4">
-                                    <img class="shadow" width="150" src="{{ asset('storage/' . $types->image) }}"
-                                        alt="{{ $types->title }}">
+                                    <img class="shadow" width="150" src="{{ asset('storage/' . $type->image) }}"
+                                        alt="{{ $type->title }}">
                                 </div>
                                 <div class="mb-3">
                                     <label for="image">Image</label>
-                                    <input type="file" class="form-control @error('image') is-invalid @enderror"
-                                        name="image" id="image" value="{{ old('image') }}">
-                                    @error('image')
+                                    <input type="file" class="form-control @error('img') is-invalid @enderror"
+                                        name="img" id="image" value="{{ old('img') }}">
+                                    @error('img')
                                         <div class="invalid-feedback">{{ $message }}</div>
                                     @enderror
                                 </div>
