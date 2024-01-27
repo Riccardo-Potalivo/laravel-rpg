6db8ff3e4aef644b2f30ef76736dfe204b71abf3
Riccardo Potalivo <riccardopotalivo@gmail.com>
2024-01-27 16:16:51 +0100

    search bar character set

--- app/Http/Controllers/Api/CharacterController.php
+++ app/Http/Controllers/Api/CharacterController.php
@@ -11,26 +11,28 @@ class CharacterController extends Controller
     //
     public function index(Request $request)
     {
+        if ($request->query('name')) {
+            $characters = Character::where('name', 'like', $request->query('name') . '%')->with(['items', 'type'])->get();
+        } else {
+            $characters = Character::with(['items', 'type'])->get();
 
+        }
 
-
-        $characters = Character::with(['items', 'type'])->get();
         return response()->json(
             [
-                'success'=>true,
-                'results'=>$characters
+                'success' => true,
+                'results' => $characters
             ]
         );
     }
 
     public function show($slug)
     {
-
         $character = Character::where('slug', $slug)->with(['items', 'type'])->first();
         return response()->json(
             [
-                'success'=>true,
-                'results'=>$character
+                'success' => true,
+                'results' => $character
             ]
         );
     }
